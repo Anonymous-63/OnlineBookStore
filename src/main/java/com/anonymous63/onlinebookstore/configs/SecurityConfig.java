8d903f86ee4e3d35725a4035b6d9edb2462e756a
SIPLwfh <mayuri@senergy.net.in>
2024-06-13 08:50:07 +0530

    documenting the api

--- src/main/java/com/anonymous63/onlinebookstore/configs/SecurityConfig.java
+++ src/main/java/com/anonymous63/onlinebookstore/configs/SecurityConfig.java
@@ -19,9 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
@@ -39,6 +41,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
--- src/main/java/com/anonymous63/onlinebookstore/security/CustomUserDetailService.java
+++ src/main/java/com/anonymous63/onlinebookstore/security/CustomUserDetailService.java
@@ -17,8 +17,7 @@ public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = this.userRepo.findByEmail(username)
+        return this.userRepo.findByEmail(username)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "email", username));
-        return user;
     }
 }
